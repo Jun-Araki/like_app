446214ed5f417ed481c7d5cb00b30d65818f9c74
Jun-Araki <jun7772006@gmail.com>
2022-09-11 09:55:50 +0900

    init＋初期データ投入

---
+++ app/models/like.rb
@@ -0,0 +1,16 @@
+class Like < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+  validates :user_id, uniqueness: {
+    scope: :post_id,
+    message: :duplicated
+  }
+
+  validate :forbid_to_owner
+
+  private
+
+  def forbid_to_owner
+    errors.add(:base, :owner_forbidden) if user_id == post.user_id
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,24 @@ email_test = "test@example.com"
 password_test = "password"
 
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
-ActiveRecord::Base.connection.execute("TRUNCATE TABLE posts RESTART IDENTITY CASCADE")
 
 user1 = User.create!(email: "satou@example.com", password: "password")
 user2 = User.create!(email: "suzuki@example.com", password: "password")
 user3 = User.create!(email: "tanaka@example.com", password: "password")
 
-user2.posts.create!(content: "沖縄そば")
-user1.posts.create!(content: "海ぶどう")
-user3.posts.create!(content: "ゴーヤチャンプル")
-user3.posts.create!(content: "サーターアンダギー")
-user1.posts.create!(content: "タコライス")
+post1 = user2.posts.create!(content: "沖縄そば")
+post2 = user1.posts.create!(content: "海ぶどう")
+post3 = user3.posts.create!(content: "ゴーヤチャンプル")
+post4 = user3.posts.create!(content: "サーターアンダギー")
+post5 = user1.posts.create!(content: "タコライス")
+
+post1.likes.create!(user_id: user1.id)
+post1.likes.create!(user_id: user3.id)
+post2.likes.create!(user_id: user2.id)
+post3.likes.create!(user_id: user1.id)
+post4.likes.create!(user_id: user1.id)
+post4.likes.create!(user_id: user2.id)
+post5.likes.create!(user_id: user3.id)
 
 # ログイン時のテストユーザー
 User.create!(email: email_test, password: password_test)
